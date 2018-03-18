6a4fff42edfa55f8a0f05bd60217b7cdc3800318
Auto Commit <daniellewis777@gmail.com>
2018-03-18 06:46:41 +0000

    Updated replaceLast.js

--- replaceLast.js
+++ replaceLast.js
@@ -1,1 +1,99 @@
-var replaceLast=function(t){var e={};function n(r){if(e[r])return e[r].exports;var u=e[r]={i:r,l:!1,exports:{}};return t[r].call(u.exports,u,u.exports,n),u.l=!0,u.exports}return n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:r})},n.r=function(t){Object.defineProperty(t,"__esModule",{value:!0})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=1)}([function(t,e,n){"use strict";t.exports=function(t,e,n){return t=""+t,arguments.length<3?t:(r=e,"[object RegExp]"===Object.prototype.toString.call(r)?function(t,e,n){var r;return r=null!=e.flags?-1===e.flags.indexOf("g")?e.flags+"g":e.flags:"g",e=new RegExp(e.source,r),t.replace(e,function(t,r,u){return 1===u.slice(r).match(e).length?n:t})}(t,e,n):function(t,e,n){var r=t.lastIndexOf(e);return r<0?t:t.substring(0,r)+n+t.substring(r+e.length,t.length)}(t,e=""+e,n));var r}},function(t,e,n){"use strict";const r=n(0);t.exports=function(t,e,n){return r(t,e,n)}}]);
+var replaceLast =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nconst replaceLast = __webpack_require__(/*! ./js/replaceLast */ \"./js/replaceLast.js\");\n\nfunction main(str, pattern, replacement) {\n  return replaceLast(str, pattern, replacement);\n}\n\nmodule.exports = main;\n\n\n//# sourceURL=webpack://replaceLast/./index.js?");
+
+/***/ }),
+
+/***/ "./js/replaceLast.js":
+/*!***************************!*\
+  !*** ./js/replaceLast.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nvar isRegex = function(any) {\n  return Object.prototype.toString.call(any) === '[object RegExp]';\n};\n\nvar regexReplaceLast = function(str, pattern, replacement) {\n  var flags;\n  /* istanbul ignore else */\n  if (pattern.flags != null) {\n    flags = (pattern.flags.indexOf('g') === -1) ? pattern.flags + 'g' : pattern.flags;\n  } else {\n    flags = 'g'; // Node 4 fix\n  }\n  pattern = new RegExp(pattern.source, flags);\n  return str.replace(pattern, function(match, offset, str) {\n    var follow = str.slice(offset);\n    var isLast = follow.match(pattern).length === 1;\n    return (isLast) ? replacement : match;\n  });\n};\n\nvar strReplaceLast = function(str, pattern, replacement) {\n  var i = str.lastIndexOf(pattern);\n  if (i < 0) return str;\n  var lhs = str.substring(0, i);\n  var rhs = str.substring(i + pattern.length, str.length);\n  return lhs + replacement + rhs;\n};\n\nvar replaceLast = function(str, pattern, replacement) {\n  str = '' + str;\n  console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX', arguments.length);\n  if (arguments.length < 3) return str;\n  if (isRegex(pattern)) {\n    return regexReplaceLast(str, pattern, replacement);\n  } else {\n    pattern = '' + pattern;\n    return strReplaceLast(str, pattern, replacement);\n  }\n};\n\nmodule.exports = replaceLast;\n\n\n//# sourceURL=webpack://replaceLast/./js/replaceLast.js?");
+
+/***/ })
+
+/******/ });
