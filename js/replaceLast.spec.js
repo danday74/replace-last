f84f49ae4cb34ea260aadc500484fc042ff13a0f
Daniel Lewis <daniellewis777@gmail.com>
2018-03-18 06:35:14 +0000

    Fixed browser tests

--- js/replaceLast.spec.js
+++ js/replaceLast.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _ = require('lodash');
+var _ = _ || require('lodash');
 var chai = chai || require('chai');
 var expect = chai.expect;
 var replaceLast = replaceLast || require('./replaceLast');
