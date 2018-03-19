e9d8dd053a57f1616a3c24c2e4931a5090360b75
Daniel Lewis <daniellewis777@gmail.com>
2018-03-19 02:35:22 +0000

    Added polyfil to istanbul ignore

--- js/replaceLast.js
+++ js/replaceLast.js
@@ -7,6 +7,7 @@ var isRegex = function(any) {
 var regexReplaceLast = function(str, pattern, replacement) {
   // Official MDN polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/flags
   // for Node.js v4
+  /* istanbul ignore next */
   if (RegExp.prototype.flags === undefined) {
     Object.defineProperty(RegExp.prototype, 'flags', {
       configurable: true,
